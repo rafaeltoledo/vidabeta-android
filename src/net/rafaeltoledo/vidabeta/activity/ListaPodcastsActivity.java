f26c947b4574503c51dc896a7ee6f2500eef8790
Rafael Toledo <rafaeldtoledo@gmail.com>
2012-08-30 12:47:07 -0300

    Adicionadas permissões de acesso à rede no Manifest, e anotações sobre quais dados devem ser obtidos do feed.

--- src/net/rafaeltoledo/vidabeta/activity/ListaPodcastsActivity.java
+++ src/net/rafaeltoledo/vidabeta/activity/ListaPodcastsActivity.java
@@ -1,5 +1,11 @@
 package net.rafaeltoledo.vidabeta.activity;
 
+import net.rafaeltoledo.vidabeta.activity.util.XMLParser;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +27,36 @@ public class ListaPodcastsActivity extends Activity {
 				finish();
 			}
 		});
+		
+		teste();
+	}
+	
+	private void teste() {
+		XMLParser parser = new XMLParser();
+		//String feed = parser.getXmlFromUrl("http://www.google.ca/reader/atom/feed/http://vidabeta.com.br/feed?hl=en&n=200");
+		String feed = parser.getXmlFromUrl("http://vidabeta.com.br/feed");
+		System.out.println(feed.length());
+		Document doc = parser.getDomElement(feed);
+		NodeList l = doc.getElementsByTagName("item");
+		System.out.println("Tamanho: " + l.getLength());
+		for (int i = 0; i < l.getLength(); i++) {
+			Node node = l.item(i);
+			NodeList children = node.getChildNodes();
+			System.out.println("Tamanho (c): " + children.getLength());
+			for (int j = 0; j < children.getLength(); j++) {
+				Node child = children.item(j);
+				System.out.println(child.getNodeName());
+			}
+		}
+		
+		/* O que me interessa:
+		 * <title>
+		 * <pubDate>
+		 * <category></category> <-- tem que ser Vida Beta Cast
+		 * <description>
+		 * <itunes:duration>
+		 * <enclosure> <-- link do Mp3
+		 * imagem? banner_post. dentro de <content:encoded>
+		 * */
 	}
 }
--- src/net/rafaeltoledo/vidabeta/activity/PrincipalActivity.java
+++ src/net/rafaeltoledo/vidabeta/activity/PrincipalActivity.java
@@ -2,9 +2,12 @@ package net.rafaeltoledo.vidabeta.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class PrincipalActivity extends Activity {
 	
@@ -28,9 +31,21 @@ public class PrincipalActivity extends Activity {
         botaoPodcasts.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-				Intent i = new Intent(PrincipalActivity.this, ListaPodcastsActivity.class);
-				startActivity(i);
+				if (redeDisponivel()) {				
+					Intent i = new Intent(PrincipalActivity.this, ListaPodcastsActivity.class);
+					startActivity(i);
+				} else {
+					Toast.makeText(PrincipalActivity.this, getApplicationContext().
+							getString(R.string.sem_rede), Toast.LENGTH_LONG).show();
+				}
 			}
 		});
+    }
+    
+    private boolean redeDisponivel() {
+    	ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		
+		return info != null;
     }
 }
